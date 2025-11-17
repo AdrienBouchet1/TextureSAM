0c6d170f4acb89825e79f5269dad1d0c350bd341
AdrienBouchet1 <adrien.bouchet@agroparistech.fr>
2025-11-17 12:16:56 +0100

    avancées

--- LatentClusteringExp.py
+++ LatentClusteringExp.py
@@ -12,7 +12,7 @@ import pandas as pd
 import argparse
 import numpy as np
 from tqdm import tqdm
-
+import shutil 
 
 
 
@@ -24,21 +24,68 @@ from tqdm import tqdm
 
 class DataSetProcessor : 
 
+        def __init__(self, input_folder) :
 
-    def __init__(self, input_folder) :
+                """
+                Args : 
+                ____
 
-        """
-        Args : 
-        ____
+                input_folder : shape of 
 
-        input_folder : shape of 
+                        input_folder/
+                                images / 
+                                        ...tif
 
-                input_folder/
-                        images / 
-                                ...tif
+                """
 
-        """
+                self.input_folder=input_folder
+                self.__prepare_folder()
 
-        self.input_folder=input_folder
+        
+        def __prepare_folder(self) : 
 
+
+                self.output_folder=os.path.join(self.input_folder,"output")
+                if os.path.exists(self.output_folder) : 
+                        shutil.rmtree(self.output_folder)
+
+                os.makedirs(self.output_folder)
+
+                self.list_images=[os.path.join(self.input_folder,"images",i) for i in os.listdir(os.path.join(self.input_folder,"images") ]
+        
+           
+        def instantiate_SAM(self) : 
+
+                        sam2 = build_sam2(self.model_cfg_path, self.model_ckpt_path, device=self.device, apply_postprocessing=False)
+
+                        self.predictor = SAM2AutomaticMaskGenerator(
+                        model=sam2,
+                        points_per_side=64,
+                        pred_iou_thresh=0.8,
+                        stability_score_thresh=0.2,
+                        mask_threshold=0.0,
+                        min_mask_region_area=0,
+                        output_mode="binary_mask",
+                        multimask_output=False
+                )
+
+
+
+
+                        
+
+
+
+        
+        
+        
+
+                
+
+
+
+
+
+        
+        
         
